fb80b2f60946e7b21e8756869b1863cb84636310
Chris <chris@cookseys.org>
2018-08-07 12:04:57 -0400

    query selected add button from index.html

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var budgetController = (function() {
 
-	var x = 23;
 
-	var add = function(a) {
-		return x + a;
-	}
 
-	return {
-		publicTest: function(b) {
-			console.log(add(b));
-		}
-	}
 
 })();
+
+
+
 
 var UIController = (function() {
 
 
 
 })();
+
+
+
+
+var appController = (function(budgetCtrl, UICtrl) {
+
+	document.querySelector('.add__btn').addEventListener('click', function() {
+
+	});
+
+})(budgetController, UIController);
